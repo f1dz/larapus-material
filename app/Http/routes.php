d6b28bcf8f7fac558665600454ba898780046015
Rahmat Awaludin <rahmat.awaludin@gmail.com>
2016-04-04 21:19:44 +0700

    daftar buku

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -33,5 +33,6 @@ Route::group(['middleware' => 'web'], function () {
     Route::get('/home', 'HomeController@index');
     Route::group(['prefix'=>'admin', 'middleware'=>['auth', 'role:admin']], function () {
         Route::resource('authors', 'AuthorsController');
+        Route::resource('books', 'BooksController');
     });
 });
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,6 +38,7 @@
                     <li><a href="{{ url('/home') }}">Dashboard</a></li>
                     @role('admin')
                     <li><a href="{{ route('admin.authors.index') }}">Penulis</a></li>
+                    <li><a href="{{ route('admin.books.index') }}">Buku</a></li>
                     @endrole
                 </ul>
 
