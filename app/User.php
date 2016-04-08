f895dd6bd1323e5f4cb30d64a54d5a7ffa867d59
Rahmat Awaludin <rahmat.awaludin@gmail.com>
2016-04-09 06:39:15 +0700

    membatasi jumlah peminjaman per buku

--- app/User.php
+++ app/User.php
@@ -4,6 +4,9 @@ namespace App;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Zizaco\Entrust\Traits\EntrustUserTrait;
+use App\Book;
+use App\BorrowLog;
+use App\Exceptions\BookException;
 
 class User extends Authenticatable
 {
@@ -26,4 +29,20 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function borrowLogs()
+    {
+        return $this->hasMany('App\BorrowLog');
+    }
+
+    public function borrow(Book $book)
+    {
+        // cek apakah buku ini sedang dipinjam oleh user
+        if($this->borrowLogs()->where('book_id',$book->id)->where('is_returned', 0)->count() > 0 ) {
+            throw new BookException("Buku $book->title sedang Anda pinjam.");
+        }
+
+        $borrowLog = BorrowLog::create(['user_id'=>$this->id, 'book_id'=>$book->id]);
+        return $borrowLog;
+    }
 }
