2d21d74ff3fdffa4e59f03367a394af9283552a9
Rahmat Awaludin <rahmat.awaludin@gmail.com>
2016-04-09 09:14:39 +0700

    query scope untuk model BorrowLog

--- app/BorrowLog.php
+++ app/BorrowLog.php
@@ -34,4 +34,14 @@ class BorrowLog extends Model
     {
         return $this->belongsTo('App\User');
     }
+
+    public function scopeReturned($query)
+    {
+        return $query->where('is_returned', 1);
+    }
+    
+    public function scopeBorrowed($query)
+    {
+        return $query->where('is_returned', 0);
+    }
 }
