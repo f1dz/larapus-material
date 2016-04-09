628ebdc8a4de6207242c4348d751f3331fbd86ba
Rahmat Awaludin <rahmat.awaludin@gmail.com>
2016-04-09 10:42:24 +0700

    custom accessor untuk field stock di model Book

--- app/Book.php
+++ app/Book.php
@@ -12,4 +12,20 @@ class Book extends Model
     {
         return $this->belongsTo('App\Author');
     }
+
+    public function borrowLogs()
+    {
+        return $this->hasMany('App\BorrowLog');   
+    }
+    
+    /**
+     * Accessor for stock attribute
+     * @return int
+     */
+    public function getStockAttribute()
+    {
+        $borrowed = $this->borrowLogs()->borrowed()->count();
+        $stock = $this->amount - $borrowed;
+        return $stock;
+    }
 }
