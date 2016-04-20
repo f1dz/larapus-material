4597cc4e31235a565a296f967695e7927fd68cf7
Rahmat Awaludin <rahmat.awaludin@gmail.com>
2016-04-20 18:46:28 +0700

    [bonus] import excel

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -17,6 +17,8 @@ use Illuminate\Support\Facades\Auth;
 use App\Exceptions\BookException;
 use Maatwebsite\Excel\Facades\Excel;
 use Barryvdh\DomPDF\Facade as PDF;
+use Illuminate\Support\Facades\Validator;
+use App\Author;
 
 class BooksController extends Controller
 {
@@ -285,7 +287,7 @@ class BooksController extends Controller
         $handler = 'export' . ucfirst($request->get('type'));
         return $this->$handler($books);
     }
-    
+
     /**
      * Download excel data buku
      * @return PHPExcel
@@ -295,18 +297,18 @@ class BooksController extends Controller
         Excel::create('Data Buku Larapus', function($excel) use ($books) {
             // Set the properties
             $excel->setTitle('Data Buku Larapus')
-                  ->setCreator('Rahmat Awaludin');
+                ->setCreator('Rahmat Awaludin');
 
             $excel->sheet('Data Buku', function($sheet) use ($books) {
                 $row = 1;
-                $sheet->row($row, [                    
+                $sheet->row($row, [
                     'Judul',
                     'Jumlah',
                     'Stok',
                     'Penulis'
                 ]);
                 foreach ($books as $book) {
-                    $sheet->row(++$row, [                    
+                    $sheet->row(++$row, [
                         $book->title,
                         $book->amount,
                         $book->stock,
@@ -326,4 +328,107 @@ class BooksController extends Controller
         $pdf = PDF::loadview('pdf.books', compact('books'));
         return $pdf->download('books.pdf');
     }
+
+    /**
+     * Generate template excel untuk import buku
+     * @return PHPExcel
+     */
+    public function generateExcelTemplate()
+    {
+        Excel::create('Template Import Buku', function($excel) {
+            // Set the properties
+            $excel->setTitle('Template Import Buku')
+                ->setCreator('Larapus')
+                ->setCompany('Larapus')
+                ->setDescription('Template import buku untuk Larapus');
+
+            $excel->sheet('Data Buku', function($sheet) {
+                $row = 1;
+                $sheet->row($row, [
+                    'judul',
+                    'penulis',
+                    'jumlah'
+                ]);
+            });
+
+        })->export('xls');
+    }
+
+    /**
+     * Import excel dan simpan ke database
+     * @return response
+     */
+    public function importExcel(Request $request)
+    {
+        // validasi untuk memastikan file yang diupload adalah excel
+        $this->validate($request, [ 'excel' => 'required|mimes:xlsx' ]);
+        // ambil file yang baru diupload
+        $excel = $request->file('excel');
+        // baca sheet pertama
+        $excels = Excel::selectSheetsByIndex(0)->load($excel, function($reader) {
+            // options, jika ada
+        })->get();
+
+        // rule untuk validasi setiap row pada file excel
+        $rowRules = [
+            'judul'   => 'required',
+            'penulis' => 'required',
+            'jumlah'  => 'required'
+        ];
+
+        // Catat semua id buku baru
+        // ID ini kita butuhkan untuk menghitung total buku yang berhasil di import
+        $books_id = [];
+
+        // looping setiap baris, mulai dari baris ke 2 (karena baris ke 1 adalah nama kolom)
+        foreach ($excels as $row) {
+            // Membuat validasi untuk row di excel
+            // Disini kita ubah baris yang sedang di proses menjadi array
+            $validator = Validator::make($row->toArray(), $rowRules);
+
+            // Skip baris ini jika tidak valid, langsung ke baris selanjutnya
+            if ($validator->fails()) continue;
+
+            // Syntax dibawah dieksekusi jika baris excel ini valid
+            
+            // Cek apakah Penulis sudah terdaftar di database
+            $author = Author::where('name', $row['penulis'])->first();
+
+            // buat penulis jika belum ada
+            if (!$author) {
+                $author = Author::create(['name'=>$row['penulis']]);
+            }
+
+            // buat buku baru
+            $book = Book::create([
+                'title'     => $row['judul'],
+                'author_id' => $author->id,
+                'amount'    => $row['jumlah']
+            ]);
+
+            // catat id dari buku yang baru dibuat
+            array_push($books_id, $book->id);
+        }
+
+        // Ambil semua buku yang baru dibuat
+        $books = Book::whereIn('id', $books_id)->get();
+
+        // redirect ke form jika tidak ada buku yang berhasil diimport
+        if ($books->count() == 0) {
+            Session::flash("flash_notification", [
+                "level"   => "danger",
+                "message" => "Tidak ada buku yang berhasil diimport."
+            ]);
+            return redirect()->back();
+        }
+
+        // set feedback
+        Session::flash("flash_notification", [
+            "level"   => "success",
+            "message" => "Berhasil mengimport " . $books->count() . " buku."
+        ]);
+
+        // Tampilkan index buku
+        return redirect()->route('admin.books.index');
+   }
 }
